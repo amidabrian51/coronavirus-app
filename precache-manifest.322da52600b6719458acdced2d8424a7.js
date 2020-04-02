02f51e55dd8ade2a86768df6293935c2e02fe50a
amidabrian51 <briankaty1@me.com>
2020-04-02 22:45:44 +0100

    Updates

---
+++ precache-manifest.322da52600b6719458acdced2d8424a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed2c85eb26fae301b6d36dee2c40b3cb",
+    "url": "/coronavirus-app/index.html"
+  },
+  {
+    "revision": "a73762d2cf2a2df69bdd",
+    "url": "/coronavirus-app/static/css/main.2b3391ba.chunk.css"
+  },
+  {
+    "revision": "7ba358a160df869f82d4",
+    "url": "/coronavirus-app/static/js/2.7e0042bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/coronavirus-app/static/js/2.7e0042bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a73762d2cf2a2df69bdd",
+    "url": "/coronavirus-app/static/js/main.316d91ad.chunk.js"
+  },
+  {
+    "revision": "ed517aebd7becc60ff49",
+    "url": "/coronavirus-app/static/js/runtime-main.9c1343ae.js"
+  }
+]);
