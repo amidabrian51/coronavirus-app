2a48f2723958c1a758df74a9fbaf06c73e331156
amidabrian51 <briankaty1@me.com>
2020-04-02 15:58:50 +0100

    first commit

---
+++ src/Countries.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+class Countries extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            countries: []
+        };
+    }
+
+    render() {
+        return <h1>Here is the app again</h1>
+    }
+}
+
+export default Countries;
